107850e8c162d9be99762ee78aa4743995579eba
Rohan Kar <rohankar@live.com>
2016-12-03 23:26:10 +0530

    Update logistic_regression.py

--- logistic_regression.py
+++ logistic_regression.py
@@ -11,8 +11,8 @@ batch_size = 100
 display_step = 1
 
 # tf Graph Input
-x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
-y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
+#x = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
+#y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
 
 # Set model weights
 W = tf.Variable(tf.zeros([784, 10]))
